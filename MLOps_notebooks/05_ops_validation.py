d6b2e2b3e656f5706ca0e681e08bba7276af363c
cicixue-db <cici.xue@databricks.com>
2022-10-18 20:25:02 -0400

    Update 05_ops_validation.py

--- MLOps_notebooks/05_ops_validation.py
+++ MLOps_notebooks/05_ops_validation.py
@@ -23,7 +23,6 @@ client = MlflowClient()
 fs = FeatureStoreClient()
 
 # After receiving payload from webhooks, use MLflow client to retrieve model details and lineage
-# Add for
 try:
   registry_event = json.loads(dbutils.widgets.get('event_message'))
   model_name = registry_event['model_name']
